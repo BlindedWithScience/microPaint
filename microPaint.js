7beeb67e5b3ca15f60f678b0b490bde124bdd197
Blind <56802446+BlindedWithScience@users.noreply.github.com>
2021-09-21 19:23:35 +0300

    update

--- microPaint.js
+++ microPaint.js
@@ -8,7 +8,14 @@ onload = () => {
 	ctx.fillStyle = 'black';
 	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-	ctx.fillStyle = 'rgba(200, 173, 25, 0.3)';
+
+	function brush (x, y) {
+		ctx.beginPath();
+		ctx.arc(x, y, 3, 0, 2 * Math.PI, true);
+		ctx.closePath();
+		ctx.fill();
+	};
+
 	
 	let draw = false;
 	let color = 'red';
@@ -31,39 +38,35 @@ onload = () => {
 		mouse.y = data.clientY - clientRect.y;
 	
 		ctx.fillStyle = color;
-		ctx.fillRect(mouse.x-5, mouse.y-5, 5, 5);
-	};
-
-
-	function getRange (x0, x, y0, y) {
-		return Math.sqrt(Math.pow((x0 - x), 2) + Math.pow((y0 - y), 2))
-	};
-
-
-	function lineInterpolation (x, x0, y, y0) {
-		for (let x_new = x0 + 1; x_new < x; x_new++) {
-			const y_new = (y - y0) * (x_new - x0) / (x - x0) + y0;
-			ctx.fillRect(x_new - 5, y_new - 5, 5, 5);
-		};
+		brush(mouse.x, mouse.y);
 	};
 
 	
-	function verticalFilling (x, y0, y) {
-		for (; y0 < y; y0++) {
-			ctx.fillRect(x - 5, y0 - 5, 5, 5);
+	function lineInterpolationForDrawing (x0, x, y0, y) {
+		const dx = x - x0;
+		const dy = y - y0;
+		const abs_dx = Math.abs(x - x0) + 1;
+		const abs_dy = Math.abs(y - y0) + 1;
+		if (abs_dy >= abs_dx) {
+			for (let i = 0; i <= abs_dy; i++) {
+				const rx = x0 + dx * i / abs_dy;
+				const ry = y0 + dy * i / abs_dy;
+				brush(rx, ry);
+			};
+		}
+		else if (abs_dx > abs_dy) {
+			for (let i = 0; i <= abs_dx; i++) {
+				const rx = x0 + dx * i / abs_dx;
+				const ry = y0 + dy * i / abs_dx;
+				brush(rx, ry);
+			};
 		};
+		console.log('error');
 	};
 
 
-	function fillVoid (x, x0, y, y0) {
-		if (x0 < x || (y0 === y && x0 < x)) {lineInterpolation(x, x0, y, y0)}
-		else if (x0 > x || (y0 === y && x0 > x)) {lineInterpolation(x0, x, y0, y)}
-		else if (y > y0) {verticalFilling(x, y0, y)}
-		else {verticalFilling(x, y, y0)}
-	};
 
-
-	canvas.onmousemove = (data) => {
+	document.onmousemove = (data) => {
 		if (draw && (data.buttons == 1)) {
 			const clientRect = canvas.getBoundingClientRect();
 			
@@ -73,12 +76,13 @@ onload = () => {
 			mouse.y = data.clientY - clientRect.y;
 			
 			ctx.fillStyle = color;
-			ctx.fillRect(mouse.x-5, mouse.y-5, 5, 5);
-			
-			if (getRange(mouse.x, mouse.x0, mouse.y, mouse.y0) > 0) {
-				console.log(mouse);
-				fillVoid(mouse.x, mouse.x0, mouse.y, mouse.y0);
+			if (mouse.x0 === undefined) {
+				brush(mouse.x, mouse.y);
+			}
+			else {
+				lineInterpolationForDrawing(mouse.x0, mouse.x, mouse.y0, mouse.y);
 			};
 		};
 	};
 };
+
