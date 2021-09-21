e86f7b7468c969be5f8c90920494f5ddc43c17e6
Blind <56802446+BlindedWithScience@users.noreply.github.com>
2021-09-21 21:17:15 +0300

    not working at all

--- microPaint.js
+++ microPaint.js
@@ -9,11 +9,13 @@ onload = () => {
 	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
 
-	function brush (x, y) {
-		ctx.beginPath();
-		ctx.arc(x, y, 3, 0, 2 * Math.PI, true);
-		ctx.closePath();
-		ctx.fill();
+	let newBrush = new Path2D();
+	newBrush.arc(x, y, 3, 0, 2 * Math.PI, true);
+	newBrush.closePath();
+
+
+	function brush (x, y, context) {
+		context.fill(newBrush)
 	};
 
 	
@@ -28,7 +30,9 @@ onload = () => {
 	};
 
 	
-	document.onmouseup = () => {draw = false};
+	document.onmouseup = () => {
+		draw = false;
+	};
 
 	canvas.onmousedown = (data) => {
 		draw = true;
@@ -38,11 +42,11 @@ onload = () => {
 		mouse.y = data.clientY - clientRect.y;
 	
 		ctx.fillStyle = color;
-		brush(mouse.x, mouse.y);
+		brush(mouse.x, mouse.y, ctx);
 	};
 
 	
-	function lineInterpolationForDrawing (x0, x, y0, y) {
+	function lineInterpolationForDrawing (x0, x, y0, y, context) {
 		const dx = x - x0;
 		const dy = y - y0;
 		const abs_dx = Math.abs(x - x0) + 1;
@@ -51,37 +55,97 @@ onload = () => {
 			for (let i = 0; i <= abs_dy; i++) {
 				const rx = x0 + dx * i / abs_dy;
 				const ry = y0 + dy * i / abs_dy;
-				brush(rx, ry);
+				brush(rx, ry, context);
 			};
 		}
 		else if (abs_dx > abs_dy) {
 			for (let i = 0; i <= abs_dx; i++) {
 				const rx = x0 + dx * i / abs_dx;
 				const ry = y0 + dy * i / abs_dx;
-				brush(rx, ry);
+				brush(rx, ry, context);
 			};
 		};
 	};
 
 
 
-	document.onmousemove = (data) => {
+	document.onmousemove = (data) => {			
 		if (draw && (data.buttons == 1)) {
 			const clientRect = canvas.getBoundingClientRect();
-			
+		
 			mouse.x0 = mouse.x;
 			mouse.y0 = mouse.y;
 			mouse.x = data.clientX - clientRect.x;
 			mouse.y = data.clientY - clientRect.y;
-			
 			ctx.fillStyle = color;
+			
 			if (mouse.x0 === undefined) {
-				brush(mouse.x, mouse.y);
+				brush(mouse.x, mouse.y, ctx);
 			}
 			else {
-				lineInterpolationForDrawing(mouse.x0, mouse.x, mouse.y0, mouse.y);
+				lineInterpolationForDrawing(mouse.x0, mouse.x, mouse.y0, mouse.y, ctx);
+			};
+		}
+		else if (draw2 && (data.buttons == 1)) {
+			const clientRect = brushEditor.getBoundingClientRect();
+		
+			mouse.x0 = mouse.x;
+			mouse.y0 = mouse.y;
+			mouse.x = data.clientX - clientRect.x;
+			mouse.y = data.clientY - clientRect.y;
+			ctx.fillStyle = color;
+
+			if (mouse.x0 === undefined) {
+				brush(mouse.x, mouse.y, ctxBrush);
+			}
+			else {
+				lineInterpolationForDrawing(mouse.x0, mouse.x, mouse.y0, mouse.y, ctxBrush);
+				newBrush.moveTo(mouse.x, mouse.y);
 			};
 		};
-	};
-};
 
+	};
+	
+
+	
+	const brushEditor = document.getElementById('BRUSH');
+
+	const ctxBrush = brushEditor.getContext('2d');
+	
+	const brushRect = brushEditor.getBoundingClientRect();
+	brushEditor.width = 100;
+	brushEditor.height = 100;
+
+
+	ctxBrush.fillStyle = 'black';
+	ctxBrush.fillRect(0, 0, ctxBrush.canvas.width, ctxBrush.canvas.height);
+	ctxBrush.fillStyle = 'red';
+	ctxBrush.beginPath();
+	ctxBrush.moveTo(50, 50);
+	ctxBrush.arc(50, 50, 2, 0, 2 * Math.PI, true);
+	ctxBrush.closePath();
+	ctxBrush.fill();
+	
+
+	let draw2 = false;
+
+
+	brushEditor.onmousedown = (data) => {
+		draw2 = true;
+		const clientRect = brushEditor.getBoundingClientRect();
+
+		mouse.x = data.clientX - clientRect.x;
+		mouse.y = data.clientY - clientRect.y;
+	
+		ctx.fillStyle = color;
+		brush(mouse.x, mouse.y, ctxBrush);
+
+		newBrush = new Path2D();
+	};
+
+	brushEditor.onmouseup = () => {
+		draw2 = false;
+		newBrush.closePath();
+	};
+		
+};
